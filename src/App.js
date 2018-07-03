9c2db8b381edb5eb14f27b713bf0d2589b39105d
Anton Samoilov <absamoilov88@gmail.com>
2018-07-03 20:51:59 +0300

    Setup basic grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
+import Favorites from './components/Favorites';
+import SearchBar from './components/SearchBar';
+import WikiBox from './components/WikiBox';
+import ArticlesBox from './components/ArticlesBox';
+import VideoBox from './components/VideoBox';
+import TwitterBox from './components/TwitterBox';
+
+import Grid from '@material-ui/core/Grid';
+
 const App = () => {
-  return <div>Working...</div>;
+  return (
+    <div>
+      <Grid container spacing={8}>
+        <Grid item xs={12}>
+          <Favorites />
+        </Grid>
+        <Grid item xs={12}>
+          <SearchBar />
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={8}>
+            <Grid item xs={6}>
+              <WikiBox />
+            </Grid>
+            <Grid item xs={6}>
+              <ArticlesBox />
+            </Grid>
+            <Grid item xs={6}>
+              <VideoBox />
+            </Grid>
+            <Grid item xs={6}>
+              <TwitterBox />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default hot(module)(App);
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 const SearchBar = () => (
-  <div>SearchBar</div>
+  <div>
+    <TextField
+      label="Search for something"
+      placeholder="Something like... World Cup 2018"
+      fullWidth
+      margin="normal"
+    />
+  </div>
 );
 
 export default SearchBar;
