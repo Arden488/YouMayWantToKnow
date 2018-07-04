bcd9774f43ddea05dc73e541adaf619e955f8b73
Anton Samoilov <absamoilov88@gmail.com>
2018-07-04 13:53:03 +0300

    Define structure for searchbar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,15 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import Grid from '@material-ui/core/Grid';
 
-const SearchBar = () => (
-  <div>
-    <TextField
-      label="Search for something"
-      placeholder="Something like... World Cup 2018"
-      fullWidth
-      margin="normal"
-    />
-  </div>
-);
+const styles = theme => ({
+  root: {
+    margin: '50px auto 100px',
+    [theme.breakpoints.up('lg')]: {
+      width: 1170,
+    },
+  },
+  input: {
+    flexGrow: 1,
+  }
+});
 
-export default SearchBar;
+function SearchBar(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <Grid container spacing={8} className={classes.root} alignItems="baseline">
+        <Grid item className={classes.input}>
+          <TextField
+            label="Search for something"
+            placeholder="Something like... World Cup 2018"
+            fullWidth
+            margin="normal"
+          />
+        </Grid>
+        <Grid item>
+          <Button 
+            variant="fab" 
+            mini
+            color="primary" 
+            aria-label="search"
+          >
+            <SearchIcon />
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+SearchBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SearchBar);
