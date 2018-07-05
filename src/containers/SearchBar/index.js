1145df86e2becc1211aca5939798ce0259495231
Anton Samoilov <absamoilov88@gmail.com>
2018-07-05 10:13:59 +0300

    Update SearchBar and move to containers

---
+++ src/containers/SearchBar/index.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+// import { compose } from 'recompose';
+
+import { fetchYoutube } from '../../actions';
+
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    margin: '50px auto 100px',
+    [theme.breakpoints.up('lg')]: {
+      width: 1170,
+    },
+  },
+  input: {
+    flexGrow: 1,
+  }
+});
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <form onSubmit={this.handleSearchSubmit}>
+          <Grid container spacing={8} className={classes.root} alignItems="baseline">
+            <Grid item className={classes.input}>
+              <TextField
+                label="Search for something"
+                placeholder="Something like... World Cup 2018"
+                fullWidth
+                margin="normal"
+                onChange={this.handleInputChange}
+              />
+            </Grid>
+            <Grid item>
+              <Button 
+                variant="fab" 
+                mini
+                color="primary" 
+                aria-label="search"
+                type="submit"
+              >
+                <SearchIcon />
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    );
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  } 
+
+  handleSearchSubmit(event) {
+    event.preventDefault();
+    this.props.fetchYoutube(this.state.value);
+  }
+}
+
+SearchBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    videos: state.videos,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { 
+    fetchYoutube: (query) => dispatch(fetchYoutube(query)),
+  };
+}
+
+export default compose(
+  withStyles(styles),
+  connect(mapStateToProps, mapDispatchToProps),
+)(SearchBar);
---
+++ src/containers/SearchBar/index.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import SearchBar from './index';
+import { createShallow, createMount } from '@material-ui/core/test-utils';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+describe('<SearchBar />', () => {
+  let store;
+  let component;
+
+  beforeAll(() => {
+    const middlewares = [ thunk ];
+    const mockStore = configureStore(middlewares);
+    store = mockStore({});
+    shallow = createShallow({ dive: true });
+    component = shallow(<SearchBar store={store} />).dive();
+  })
+
+  it('renders correctly', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it('changes state on value change', () => {
+    const value = 'Test';
+
+    component.find('TextField').simulate('change', {
+      target: {
+        value
+      }
+    });
+
+    expect(component.state().value).toEqual(value);
+  });
+
+  it('submits form', () => {
+    const value = 'Test';
+    
+    component.find('TextField').simulate('change', {
+      target: {
+        value
+      }
+    });
+
+    component.find('form').simulate('submit', {
+      preventDefault: jest.fn()
+    });
+  });
+});
