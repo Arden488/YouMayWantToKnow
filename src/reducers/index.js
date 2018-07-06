a3a159fee7ab9f313bd166a448303ce69f53ecec
Anton Samoilov <absamoilov88@gmail.com>
2018-07-06 15:22:58 +0300

    Update rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import VideoReducer from './reducer_video';
 import WikiReducer from './reducer_wiki';
 import FlickrReducer from './reducer_flickr';
+import ArticlesReducer from './reducer_articles';
 
 const rootReducer = combineReducers({
   videos: VideoReducer,
   wikiData: WikiReducer,
   photos: FlickrReducer,
+  articles: ArticlesReducer,
 });
 
 export default rootReducer;
