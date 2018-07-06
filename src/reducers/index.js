945c5e11f641cfd31408071899e0be80604cb941
Anton Samoilov <absamoilov88@gmail.com>
2018-07-06 15:03:42 +0300

    Connect all reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 
 import VideoReducer from './reducer_video';
+import WikiReducer from './reducer_wiki';
+import FlickrReducer from './reducer_flickr';
 
 const rootReducer = combineReducers({
   videos: VideoReducer,
+  wikiData: WikiReducer,
+  photos: FlickrReducer,
 });
 
 export default rootReducer;
