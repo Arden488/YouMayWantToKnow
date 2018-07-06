9604c6d798fcadf3e8d78fd41dde2336b142456c
Anton Samoilov <absamoilov88@gmail.com>
2018-07-06 15:01:20 +0300

    Update video reducer

--- src/reducers/reducer_video.js
+++ src/reducers/reducer_video.js
@@ -1,7 +1,6 @@
 export default function(state = [], action) {
   switch(action.type) {
     case 'FETCH_YOUTUBE':
-      console.log(action.payload.data.items);
       return action.payload.data.items;
   }
 
