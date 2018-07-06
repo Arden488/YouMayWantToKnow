d26c9eec10d0ab7631395258e45c564d4b24b096
Anton Samoilov <absamoilov88@gmail.com>
2018-07-06 15:04:51 +0300

    Update favorites component

--- src/components/Favorites/index.js
+++ src/components/Favorites/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
-const Favorites = () => (
-  <div>Favorites</div>
-);
+import Chip from '@material-ui/core/Chip';
 
-export default Favorites;
+const styles = theme => ({
+  chip: {
+    margin: theme.spacing.unit / 2,
+  }
+});
+
+function Favorites(props) {
+  const { classes } = props;
+  
+  return (
+    <div>
+      <Chip className={classes.chip} label="SpaceX" clickable />
+      <Chip className={classes.chip} label="World Cup 2018" clickable />
+      <Chip className={classes.chip} label="Hawking" clickable />
+    </div>
+  );
+}
+
+Favorites.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Favorites);
